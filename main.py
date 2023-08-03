267c9e8634df1d955c70c419506b5ba5a94de6ef
wangzhefeng <wangzhefengr@163.com>
2023-08-03 23:23:58 +0800

    init project

---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+# ***************************************************
+# * File        : main.py
+# * Author      : Zhefeng Wang
+# * Email       : wangzhefengr@163.com
+# * Date        : 2023-08-03
+# * Version     : 0.1.080320
+# * Description : description
+# * Link        : link
+# * Requirement : 相关模块版本需求(例如: numpy >= 2.1.0)
+# ***************************************************
+
+# python libraries
+import os
+import sys
+ROOT = os.getcwd()
+if str(ROOT) not in sys.path:
+    sys.path.append(str(ROOT))
+
+import arviz as az
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# global variable
+LOGGING_LABEL = __file__.split('/')[-1][:-3]
+
+
+
+
+
+
+
+# 测试代码 main 函数
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
